88437c1ddca760fbb1ce3f53b7ebc8e68de78bde
aali9 <74592306+aali9@users.noreply.github.com>
2022-05-18 22:39:07 +0100

    local storage

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,24 @@ var CurrentHR = moment().format("h");
 var date = moment().format("MMMM Do, YYYY");
 $("#currentDay").text(date);
 
+// LOCAL STORAGE FUNCTION ------------------------------------------------------------------------------
+
+$(document).ready(function () {
+  // Save buttons job
+  $(".saveBtn").on("click", function () {
+    // Catch the value of 3rd 'dom' down, which is the user input value/class called description
+    var myText = $(this).siblings(".description").val();
+
+    // Catch the id of the 1st 'dom', which is the times, explained in the lines below(34-43)
+    var timings = $(this).parent().attr("id");
+
+    // Below saves users input on the planner even after refreshing in local storage..
+    localStorage.setItem(timings, myText);
+
+    // Logging the activity
+    console.log("button is clicked");
+    console.log(myText);
+    console.log(timings);
+  });
+
+  
