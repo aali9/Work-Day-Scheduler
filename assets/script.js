98a2a665b4746055b95ef7e427436671d043a7b1
aali9 <74592306+aali9@users.noreply.github.com>
2022-05-18 22:40:44 +0100

    color tense

--- assets/script.js
+++ assets/script.js
@@ -30,4 +30,38 @@ $(document).ready(function () {
     console.log(timings);
   });
 
+  $("#9am .description").val(localStorage.getItem("9am"));
+  $("#10am .description").val(localStorage.getItem("10am"));
+  $("#11am .description").val(localStorage.getItem("11am"));
+  $("#12pm .description").val(localStorage.getItem("12pm"));
+  $("#1pm .description").val(localStorage.getItem("1pm"));
+  $("#2pm .description").val(localStorage.getItem("2pm"));
+  $("#3pm .description").val(localStorage.getItem("3pm"));
+  $("#4pm .description").val(localStorage.getItem("4pm"));
+  $("#5pm .description").val(localStorage.getItem("5pm"));
+  $("#6pm .description").val(localStorage.getItem("6pm"));
+});
+
+// color code based on past, current, future tense
+
+// Each 'input' box will get colour coded based on the below function
+DescriptionBox.each(function (Color) {
+    // "color" is 0, if "0" and counting + 9 is less than current hour, then create class PAST...
+    if (CurrentHR > Color + 9) {
+      // Find the 'past' class and colour GREY - CSS
+      $(this).addClass("past");
+    }
   
+    // If the current hour is equals to hours counting it is PRESENT...
+    if (CurrentHR == Color + 9) {
+      // Find the 'present' class and colour RED - CSS
+      $(this).addClass("present");
+    }
+  
+    // If counting number + 9 is larger than current hour, then create class FUTURE...
+    if (CurrentHR < Color + 9) {
+      // Find the 'future' class and colour GREEN - CSS
+      $(this).addClass("future");
+    }
+  });
+  console.log(CurrentHR);
